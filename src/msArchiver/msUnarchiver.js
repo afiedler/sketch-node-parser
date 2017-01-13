704d8246fa5b25721c063ca0d44a4243eb6869c8
Shawn Allen <shawn.allen@gsa.gov>
2017-01-12 16:51:21 -0800

    use ES2015 class, warn for no deserializer

--- src/msArchiver/msUnarchiver.js
+++ src/msArchiver/msUnarchiver.js
@@ -1,34 +1,39 @@
-var sketchClasses = require('./sketchClasses');
-const _ = require('lodash');
+'use strict';
+const sketchClasses = require('./sketchClasses');
 
-function MSUnarchiver(archive) {
-  this._archive = archive;
-}
+module.exports = class MSUnarchiver {
 
-MSUnarchiver.prototype.top = function() {
-  return this.deserializeByRef(this._archive.$top.root);
-};
+  constructor(archive) {
+    this._archive = archive;
+  }
 
-MSUnarchiver.prototype.getByRef = function(ref) {
-  return this._archive.$objects[ref];
-};
+  get top() {
+    return this.deserializeByRef(this._archive.$top.root);
+  }
 
-MSUnarchiver.prototype.deserialize = function(obj) {
-  if(obj.$class) {
-    var classname = this.getByRef(obj.$class).$classname;
-    return new sketchClasses[classname](obj, this);
-  } else if(obj === '$null') {
-    return null;
-  } else if(_.isString(obj)) {
-    return obj;
-  } else {
-    throw new Error('not able to deserialize');
+  getByRef(ref) {
+    return this._archive.$objects[ref];
+  }
+
+  deserialize(obj) {
+    if (obj.$class) {
+      var classname = this.getByRef(obj.$class).$classname;
+      if (classname in sketchClasses) {
+        return new sketchClasses[classname](obj, this);
+      } else {
+        console.warn('no deserializer class for:', classname);
+        return obj;
+      }
+    } else if (obj === '$null') {
+      return null;
+    } else if (typeof obj === 'string') {
+      return obj;
+    } else {
+      throw new Error('not able to deserialize');
+    }
+  }
+
+  deserializeByRef(ref) {
+    return this.deserialize(this.getByRef(ref));
   }
 };
-
-MSUnarchiver.prototype.deserializeByRef = function(ref) {
-  return this.deserialize(this.getByRef(ref));
-};
-
-
-module.exports = MSUnarchiver;
