26acfcbaa039c9bb7bbeb79b8fdd9676b0c43ab0
Shawn Allen <shawn.allen@gsa.gov>
2017-01-12 17:14:50 -0800

    rename top -> root; add recursive deserialization

--- src/MSUnarchiver.js
+++ src/MSUnarchiver.js
@@ -5,10 +5,11 @@ module.exports = class MSUnarchiver {
 
   constructor(archive) {
     this._archive = archive;
+    this._root = this._archive.$top.root;
   }
 
-  get top() {
-    return this.deserializeByRef(this._archive.$top.root);
+  get root() {
+    return this.deserializeByRef(this._root);
   }
 
   getByRef(ref) {
@@ -22,18 +23,31 @@ module.exports = class MSUnarchiver {
         return new sketchClasses[classname](obj, this);
       } else {
         console.warn('no deserializer class for:', classname);
-        return obj;
+        return this.deserializeAll(obj, {'$classname': classname});
       }
     } else if (obj === '$null') {
       return null;
     } else if (typeof obj === 'string') {
       return obj;
     } else {
-      throw new Error('not able to deserialize');
+      throw new Error('not able to deserialize: ' + JSON.stringify(obj));
     }
   }
 
   deserializeByRef(ref) {
     return this.deserialize(this.getByRef(ref));
+  }
+
+  deserializeAll(obj, into) {
+    const dest = into || {};
+    Object.keys(obj).forEach(key => {
+      if (key.charAt(0) !== '$') {
+        const indexOrValue = obj[key];
+        dest[key] = (indexOrValue === this._root)
+          ? indexOrValue
+          : this.deserializeByRef(val);
+      }
+    });
+    return dest;
   }
 };
